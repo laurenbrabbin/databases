8da705fc043e9120f3ad466fbf9b7322e5dd02e0
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-10-11 09:01:24 +0100

    adding update method to shop manager project

--- shop-manager/lib/item_repository.rb
+++ shop-manager/lib/item_repository.rb
@@ -31,4 +31,31 @@ class ItemRepository
 
     return nil
   end
+  def find(id)
+    sql = 'SELECT id, name, unit_price, quantity FROM items WHERE id = $1;' 
+    sql_params = [id] 
+
+    result_set = DatabaseConnection.exec_params(sql,sql_params) #sending both the sql and params
+    
+    record = result_set[0]
+    
+
+    item = Item.new
+    item.id = record['id']
+    item.name = record['name']
+    item.unit_price = record['unit_price']
+    item.quantity = record['quantity']
+
+    return item
+  end
+  
+
+  def update(item)
+    sql = "UPDATE items SET name = $1, unit_price = $2, quantity = $3 WHERE id = $4;"
+    sql_params = [item.name, item.unit_price, item.quantity, item.id]
+
+    DatabaseConnection.exec_params(sql, sql_params)
+
+    return nil
+  end
 end
